5226668e4f0117d1361ddc36869e28fcb48a9100
avinashbiradar <avinashbiradar179@gmail.com>
2020-11-15 19:29:16 +0530

    Added:use case 2 is added

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,10 +1,15 @@
-echo "Welcome to Employee Wage Computation Program"
-
-ispresent=1
-randomcheck=$((RANDOM%2))
-if [[ $ispresent -eq $randomcheck ]]
+isPresent=1
+random=$(( RANDOM%2 ))
+echo "Random number: "$random
+if(( $random == $isPresent ))
 then
-      echo "Employee is Present"
+echo "Employee is Present!"
+empHrs=8
+ratePerHour=20
+salary=$(( $empHrs * $ratePerHour))
 else
-      echo "Employee is Absent"
+echo "Employee is absent!"
+salary=0
 fi
+
+echo "Salary: $salary"
