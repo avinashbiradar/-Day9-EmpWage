f21e58e98f1896aea9666f468faa8f67312b2090
avinashbiradar <avinashbiradar179@gmail.com>
2020-11-15 20:03:00 +0530

    Added:use case 5 is added

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -8,6 +8,7 @@ then
 else
       echo "Employee is Absent"
 fi
+echo"************************************************************"
 
 isPresent=1
 random=$(( RANDOM%2 ))
@@ -24,6 +25,8 @@ salary=0
 fi
 
 echo "Salary: $salary"
+
+echo"**********************************************************"
 
 IS_PART_TIME=1
 IS_FULL_TIME=2
@@ -43,3 +46,26 @@ echo "Employee hours: " $empHrs
 salary=$(( $empHrs * $ EMP_RATE_PER_HR ))
 
 echo "Salary: $salary"
+
+echo"*****************************************************"
+IS_PART_TIME=1
+IS_FULL_TIME=2
+EMP_RATE_PER_HR=20
+NUM_WORKING_DAYS=20
+totalSalary=0
+for(( day=1 ; day <= $NUM_WORKING_DAYS; day++ ))
+do
+random=$(( RANDOM%3 ))
+case $random in
+$IS_PART_TIME) empHrs=4
+;;
+$IS_FULL_TIME) empHrs=8
+;;
+*) empHrs=0
+esac
+salary=$(( $empHrs * $ EMP_RATE_PER_HR))
+echo "Day: $day Salary: $salary"
+totalSalary=$(( $totalSalary + $salary ))
+done
+
+echo "Total Salary: $totalSalary"
